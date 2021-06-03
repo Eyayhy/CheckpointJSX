0c8099ecd863cb8711b82ef703000e5a53cf8679
eya <eya@1D-EYA.DC-CAPES.COM>
2021-06-04 00:34:06 +0100

    checkpoint react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import imageSrc from "./imageInSrc.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <div style ={{border:"solid 1px black" , maxWidth:"100vw"}} >
+   <h1 className="title red">Eya</h1>
+   <br>
+   </br>
+   <img className="public-img" src={imageSrc} alt='imageSrc'/>
+  <br>
+  </br>
+  <img className="src-img" src="/imageInPublic.jpg" alt="imagePublic" />
+
+  </div>
+  <video width={320} height={240} controls>
+
+ <source src="/myVideo.mp4" type="video/mp4" /> 
+  </video> 
+      
+    </>
   );
 }
 
